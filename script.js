fbdf7f49e0438e9349d46c8b0e3349577348687e
Tobi <tobi.riemenschneider@gmail.com>
2016-12-27 20:43:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.sec-hand');
+const minutesHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
+function setDate() {
+	const now = new Date();
+
+	const seconds = now.getSeconds();
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const minutes = now.getMinutes();
+	const minutesDegrees = ((minutes / 60) * 360) + 90;
+	minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = ((hours / 12) * 360) + 90;
+	hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+	console.log(minutes);
+}
+
+setInterval(setDate, 1000)
